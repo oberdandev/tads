f0f2aad39bb9b55fd9ae3c5a8c8c8970ada96454
Oberdan <oberdan.dev@gmail.com>
2023-03-21 15:35:36 -0400

    Setup folder

---
+++ ATP - Algoritmos e Técnicas de Programação/1-Diagramas.js
@@ -0,0 +1,15 @@
+const number = 11111111111
+
+const isPrimeNumber = (number) => {
+    let divisores = 0;
+    
+    for(let i = 1; i < number; i++){
+      if(divisores > 2) break;
+      if(number % i === 0) divisores++ ;
+    }
+
+    if(divisores > 2) console.log(number, 'Não é primo');
+    else console.log(number, 'É primo');
+}
+
+isPrimeNumber(number) 
